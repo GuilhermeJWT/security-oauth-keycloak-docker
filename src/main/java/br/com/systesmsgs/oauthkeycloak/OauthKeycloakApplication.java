47accabc89059763d657931c4e37a87ba34b796b
GuilhermeJWT <guiromanno@gmail.com>
2021-05-29 17:04:05 -0300

    Setup Inicial

---
+++ src/main/java/br/com/systesmsgs/oauthkeycloak/OauthKeycloakApplication.java
@@ -0,0 +1,13 @@
+package br.com.systesmsgs.oauthkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthKeycloakApplication.class, args);
+	}
+
+}
